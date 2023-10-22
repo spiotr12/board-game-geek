12fcdba71c8b058f69e45b07aa5a6457c8687a46
FreyzerFault <martinezalcantara29@gmail.com>
2023-10-22 14:43:28 +0200

    Added Collection Response Model & Collection Model

---
+++ src/models/bgg-collection-response-model.ts
@@ -0,0 +1,29 @@
+import { IAttributes } from './attributes.interface';
+import { BggCollectionItem, IBggCollectionItem } from './bgg-collection-model';
+
+export interface IBggCollectionResponse {
+  _declaration: IAttributes<{
+    version: string;
+    encoding: string;
+  }>;
+  items: {
+    item: IBggCollectionItem[];
+  } & IAttributes<{
+    totalitems: string;
+    termsofuse: string;
+    pubdate: string;
+  }>;
+}
+export class BggCollectionResponse {
+  items: BggCollectionItem[];
+  totalitems: number;
+
+  constructor(data: IBggCollectionResponse) {
+    if (Array.isArray(data.items.item)) {
+      this.items = data.items.item.map((item) => new BggCollectionItem(item));
+    } else {
+      this.items = [new BggCollectionItem(data.items.item)];
+    }
+    this.totalitems = parseInt(data.items._attributes.totalitems);
+  }
+}
